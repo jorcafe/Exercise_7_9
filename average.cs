f5a4b1d0e0acbc661b2e822517f57e8b72299fa7
jorcafe <74430884+jorcafe@users.noreply.github.com>
2020-11-20 20:09:05 +0100

    Update average.cs

--- average.cs
+++ average.cs
@@ -1,24 +1,26 @@
 using System;
+
 public class Average 
 {
 	public static void Main()
 	{
-	double a=0, b, c=0, average;
-	string s;
-	Console.Write("Insert a mark: ");
-	s=Console.ReadLine();		 
-	while(s!="end")
-		{  
-		b=Convert.ToDouble(s);	
-		c++;   //counter of numbers
-		a+=b;  // sum of all the notes
+		double a=0, b, c=0, average;
+		string s;
 		Console.Write("Insert a mark: ");
-		s=Console.ReadLine();
+		s=Console.ReadLine();		 
+		
+		while(s!="end")
+		{  
+			b=Convert.ToDouble(s);	
+			c++;   //counter of numbers
+			a+=b;  // sum of all the notes
+			Console.Write("Insert a mark: ");
+			s=Console.ReadLine();
 		}
-	if(c!=0)
+		if(c!=0)
 		{
-		average=a/c;
-		Console.WriteLine("The average is {0}.",average);
+			average=a/c;
+			Console.WriteLine("The average is {0}.",average);
 		}
 	}
 }
